1b6d1c51cf224d0a2ad8fe1b71bd028228a0e726
Sean Genabe <seangenabe@outlook.com>
2017-01-01 04:27:18 +0800

    changed `spaced-comment` to warn

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ module.exports = {
     "space-in-parens": 2,
     "space-infix-ops": 2,
     "space-unary-ops": [2, { "words": true, "nonwords": false }],
-    "spaced-comment": 2,
+    "spaced-comment": 1,
     "unicode-bom": 2,
     "arrow-spacing": 2,
     "constructor-super": 2,
